39f3d4a266b433478df50e4c2ee58330bd59dd74
Nico Kreiling <nico.kreiling@gmail.com>
2017-05-17 18:52:18 +0200

    imporve layout and boost more important fields

--- run.py
+++ run.py
@@ -8,95 +8,84 @@ import datetime
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+def getCheatSheets():
+    host = 'http://35.157.154.191:9200'
 
-def search_key_for_name(emp):
-    """Generate a string search key for a emp"""
-    elements = []
-    elements.append(emp['firstName'])  # emp tags
-    elements.append(emp['lastName'])  # title of emp
-    try:
-        elements.append(emp['location'])  # location of emp
-        pass
-    except TypeError as e:
-        elements.append("")  # emp tags
-    try:
-        elements.append(emp['lob'])  # lob of emp
-        pass
-    except TypeError as e:
-        elements.append("")  # emp tags
-
-    return u' '.join(str(elements))
-
-
-def search_by_time(emp):
-    """Generate a string search key for a emp"""
-    elements = []
-    try:
-        elements.append(str(emp['admission'][:7]))  # emp tags
-        pass
-    except TypeError as e:
-        elements.append("")  # emp tags
-    return u' '.join(elements)
-
-def search_by_skill(emp):
-    """Generate a string search key for a emp"""
-    elements = []
-    return u' '.join(map(lambda x: x['name'],emp['skills']))
-
-
-def main(wf):
-
-    def getCheatSheets():
-        host = 'http://35.157.154.191:9200'
-
-        payload = {
-           "query": {
-                "query_string" : {
-                    "fields" : ["file","content","section"],
-                    "query" : str(wf.args[0])
-                }
+    payload = {
+       "query": {
+            "query_string" : {
+                "fields" : ["code^3","comment^2","file^2","content","section"],
+                "query" : str(wf.args[0])
             }
         }
+    }
 
-        # json_data = json.dumps(data)
-        cheatSheets = json.loads(requests.post(host+"/cheatsheets/markdown/_search", data=json.dumps(payload)).text)['hits']['hits']
-        return cheatSheets
+    # json_data = json.dumps(data)
+    cheatSheets = json.loads(requests.post(host+"/cheatsheets/markdown/_search", data=json.dumps(payload)).text)['hits']['hits']
+    return cheatSheets
 
+def parseElement(cmd):
+    src = cmd['_source']
+    item = {'title':"",'subtitle':"",'icon':"",'file':"",'link':"" }
+    item["link"] = 'https://gitlab.com/kreiling/cheatsheets/blob/master/{}'.format(src['link'])
+    item["id"] = cmd['_id']
+    item["code"] = "invalid"
+    item["title"] = "invalid"
+    item["subtitle"] = "invalid"
+    try:
+        # if hasattr(src, 'comment'):
+        if (src['tag']=='code' and str(src['info']).strip()=='sh'):
+            item["code"] = src['code']
+            item["title"]=src['code']
+            item["subtitle"] = '{} ({})'.format(src['comment'],cmd['_score'])
+        else:
+            item["title"]=src['content']
+            item["subtitle"] = '{} ({})'.format(src['file'],cmd['_score'])
+            item["code"] = src['content']
+    except Exception as e:
+        item["code"] = "invalid code"
+        item["title"] = "invalid entry"
+        item["subtitle"] = src['content']
+
+    item["icon"] = src['file'].split(".",1)[0]
+    return item;
+
+def main(wf):
     cheatSheets = getCheatSheets()
-    # print("type", file=sys.stderr)
-    # wf.logger.debug(type(cheatSheets))
-
-    regex = re.compile('[A-Za-z]+')
     # Loop through the returned posts and add an item for each to
     # the list of results for Alfred
     for cmd in cheatSheets:
-        src = cmd['_source']
-        link = 'https://gitlab.com/kreiling/cheatsheets/blob/master/{}'.format(src['link'])
-        id = cmd['_id']
-        if hasattr(src, 'comment'):
-            subtitle = '{} ({})'.format(src['comment'],cmd['_score'])
-        else:
-            subtitle = '{} ({})'.format(src['file'],cmd['_score'])
-        wf.add_item(title=src['content'],
-                    # icontype=u'fileicon',
-                    icon=u'imgs/{}.png'.format(src['file'].split(".",1)[0]),
-                    subtitle=subtitle,
-                    # arg=src['link'],
-                    arg="{} {}".format(link,id),
-                    valid=True)
+        try:
+            item = parseElement(cmd)
+            wf.add_item(
+                title=item["title"],
+                icon=u'imgs/{}.png'.format(item["icon"]),
+                subtitle=item["subtitle"],
+                # Pass multiple arguments, so that workflow can use the correct, depending on pressed keys
+                # link => open GitLab Documentation
+                # id => open ElasticSearch Document
+                arg="{} {} {}".format(item["link"],item["id"], item["code"]),
+                valid=True)
 
-    # Send the results to Alfred as XML
+
+        # Send the results to Alfred as XML
+        except Exception as e:
+            link = 'http://35.157.154.191:9200/cheatsheets/markdown/_search?pretty&q=section:{}&q=content:{}&q=file:{}&default_operator=or'.format(wf.args[0],wf.args[0],wf.args[0])
+            wf.add_item(title="Searched for:"+str(wf.args[0]),
+                        subtitle="Exception Message: "+str(e),
+                        arg="{} {} \"{}\"".format(link,"test", e),
+                        valid=True)
     wf.send_feedback()
 
 
 if __name__ == u"__main__":
     wf = Workflow( update_settings={
         # Your username and the workflow's repo's name
-        'github_slug': 'nik-ffm/inca-workflow',
+        # 'github_slug': 'nik-ffm/inca-workflow',
         # Optional number of days between checks for updates
-        'frequency': 1
+        # 'frequency': 1
     })
     # Install update if available
-    if wf.update_available:
-        wf.start_update()
+    # if wf.update_available:
+    #     wf.start_update()
     sys.exit(wf.run(main))
